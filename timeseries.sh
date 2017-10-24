d2af31e9d88ef771073caab5185c662e85b47173
fixone <fixone@users.noreply.github.com>
2017-10-24 22:01:13 +0300

    Update timeseries.sh

--- timeseries.sh
+++ timeseries.sh
@@ -3,9 +3,9 @@ if [ -d "/data" ]; then
         cd /data
 fi
 
-echo "getting the file.."
+echo "getting the file ...."
 wget -q https://raw.githubusercontent.com/academyofdata/data/master/timeseries.csv.gz
-echo "unzipping it..."
+echo "unzipping it ...."
 gunzip timeseries.csv.gz
-echo "splitting it..."
+echo "splitting it ...."
 split -d -l 216000 --additional-suffix .csv timeseries.csv ts_part
