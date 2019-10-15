8bdf4910a78752db77c253b23c02ae39c6f5d705
Felix <fixone@users.noreply.github.com>
2019-10-15 23:19:26 +0300

    Create get-data.sh

---
+++ timeseries/get-data.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+ODIR="/tmp/timeseries"
+sudo mkdir -p /data/timeseries 2>>/dev/null
+if [ -d "/data" ]; then
+        ODIR="/data/timeseries"
+else
+        mkdir -p /tmp/timeseries
+fi
+# use wget to download the files
+sudo wget https://raw.githubusercontent.com/academyofdata/data/timeseries/master/ts-aa.csv.gz -O $ODIR/ts-aa.csv.gz
+sudo wget https://raw.githubusercontent.com/academyofdata/data/timeseries/master/ts-ab.csv.gz -O $ODIR/ts-ab.csv.gz
+sudo wget https://raw.githubusercontent.com/academyofdata/data/timeseries/master/ts-ac.csv.gz -O $ODIR/ts-ac.csv.gz
+sudo wget https://raw.githubusercontent.com/academyofdata/data/timeseries/master/ts-ad.csv.gz -O $ODIR/ts-ad.csv.gz
+sudo wget https://raw.githubusercontent.com/academyofdata/data/timeseries/master/ts-ae.csv.gz -O $ODIR/ts-ae.csv.gz
+sudo wget https://raw.githubusercontent.com/academyofdata/data/timeseries/master/ts-af.csv.gz -O $ODIR/ts-af.csv.gz
+sudo wget https://raw.githubusercontent.com/academyofdata/data/timeseries/master/ts-ag.csv.gz -O $ODIR/ts-ag.csv.gz
+sudo wget https://raw.githubusercontent.com/academyofdata/data/timeseries/master/ts-ah.csv.gz -O $ODIR/ts-ah.csv.gz
+sudo wget https://raw.githubusercontent.com/academyofdata/data/timeseries/master/ts-ai.csv.gz -O $ODIR/ts-ai.csv.gz
+sudo wget https://raw.githubusercontent.com/academyofdata/data/timeseries/master/ts-aj.csv.gz -O $ODIR/ts-aj.csv.gz
+
+sudo gunzip -f $ODIR/ts-aa.csv.gz
+sudo gunzip -f $ODIR/ts-ab.csv.gz
+sudo gunzip -f $ODIR/ts-ac.csv.gz
+sudo gunzip -f $ODIR/ts-ad.csv.gz
+sudo gunzip -f $ODIR/ts-ae.csv.gz
+sudo gunzip -f $ODIR/ts-af.csv.gz
+sudo gunzip -f $ODIR/ts-ag.csv.gz
+sudo gunzip -f $ODIR/ts-ah.csv.gz
+sudo gunzip -f $ODIR/ts-ai.csv.gz
+sudo gunzip -f $ODIR/ts-aj.csv.gz
